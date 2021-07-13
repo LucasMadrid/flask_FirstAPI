1b27a39f70513c556bd2d17eb26a16657f074ded
Lucas Madrid <madridlucas75@gmail.com>
2021-07-13 00:04:01 -0300

    Index.html modified to dynamic display task obj with jinja; Index function content added to save a new task to the db and show the task that are active

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, datetime
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class toDo(db.Model):
+class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
@@ -18,8 +18,20 @@ class toDo(db.Model):
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = ToDo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            
+            return redirect('/')
+        except:
+            return 'there was an issue trying to save your task to the DB'
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
